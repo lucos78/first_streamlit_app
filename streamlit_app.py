e770437ba0b3b7a75493cd61015684ef501f7639
Luc POTTIER <59259709+lucos78@users.noreply.github.com>
2022-04-12 21:17:35 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,4 +40,7 @@ import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT FRUIT_NAME FROM FRUIT_LOAD_LIST")
-streamlit.dataframe(my_cur, columns = ('FRUIT'))
+df = pd.DataFrame(
+    my_cur,
+    columns=('FRUIT %d' % i for i in range(20)))
+streamlit.dataframe(df)
