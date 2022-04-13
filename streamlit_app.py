00a20d8e49a3009eebacc634e3675498590d43d3
Luc POTTIER <59259709+lucos78@users.noreply.github.com>
2022-04-13 10:10:51 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,20 +26,22 @@ fruit_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruit_to_show)
 
 #New section to display fruityvice api response
-streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like infotmation about ?', 'Kiwi')
-streamlit.write('The user entered', fruit_choice)
-
 #import requests
-
-fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#streamlit.text(fruityvice_response.json()) #just writes data to the screen
-
-#take the json version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#output it the screen as table
-streamlit.dataframe(fruityvice_normalized)
-
+streamlit.header('Fruityvice Fruit Advice!')
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like infotmation about ?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    #take the json version of the response and normalize it
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    #output it the screen as table
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error
+  
+#stop le chargement de la page
 streamlit.stop()
 
 
